ca21f198a386937ace4c69a33afcfcbc47a47088
huy10 <huy0111@gmail.com>
2014-03-14 10:24:30 +0800

    Create Resource.h

---
+++ Resource.h
@@ -0,0 +1,54 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ChcrDemo.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CHCRDEMO_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define IDR_CHCRDEMO_MENU               129
+#define IDC_DRAW_AREA                   1000
+#define IDC_EDIT_TEXT                   1001
+#define IDC_LIST_CANDIDATE              1002
+#define IDC_EDIT_RANGE                  1003
+#define IDC_LIST_CANDIDATE2             1003
+#define IDC_BUTTON1                     1004
+#define IDC_BUTTON2                     1005
+#define IDC_BUTTON_SAVETXT              1006
+#define IDC_BUTTON_SAVEPWT              1007
+#define IDC_BUTTON_SAVEPWT2             1008
+#define IDC_BUTTON_NEXTPWT              1008
+#define IDC_CHECK_GE_PUNC               1010
+#define IDC_CHECK5                      1011
+#define IDC_CHECK_NG_PUNC               1011
+#define IDC_CHECK1                      1013
+#define IDC_CHECK_SAVEFILE              1013
+#define IDC_COMBO1                      1014
+#define IDC_COMBO_LANG                  1014
+#define IDC_EDIT1                       1015
+#define IDC_EDIT2                       1017
+#define IDC_EDIT_SAMFILENAME            1017
+#define IDC_EDIT3                       1018
+#define IDC_EDIT_CORRLT                 1018
+#define ID_OPTION                       32771
+#define IDM_OPTION                      32772
+#define ID_CLEAR                        32773
+#define IDM_CLEAR                       32774
+#define ID_BATCH                        32775
+#define IDM_BATCH                       32776
+#define ID_32777                        32777
+#define IDM_LOAD_PWT                    32778
+#define ID_Menu                         32779
+#define IDM_LOAD_FILE                   32780
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32781
+#define _APS_NEXT_CONTROL_VALUE         1019
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
